2466820f8af40d639cd01570ba8846487bccc431
vipulkhutale12 <vipulkhutale1212>
2020-09-20 16:10:12 +0530

    java

---
+++ src/com/programm/NestedIf.java
@@ -0,0 +1,21 @@
+package com.programm;
+
+public class NestedIf {
+
+	public static void main(String[] args) {
+		//  To check the user is eligible for blood donate or Not
+
+		int weight=52;
+		int age=34;
+
+		if(age>=18)
+		{
+			if(weight>=50)
+			{
+				System.out.println("You are eligible for blood donate");
+			}
+		}
+
+	}
+
+}
